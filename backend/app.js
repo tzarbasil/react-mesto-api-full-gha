0fd89f4942f181660485325b9dee49ffe3e13a44
tzarbasil <atreides03@mail.ru>
2023-08-20 10:58:25 +0200

    CORS 2

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,6 @@ app.use((err, req, res, next) => {
 
 // app.use(helmet());
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
@@ -53,12 +52,24 @@ const allowedCors = [
   'localhost:3000'
 ];
 
-app.use(function(req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
+const { method } = req;
+
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+app.use(function (req, res, next) {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
   if (allowedCors.includes(origin)) {
-      // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-      res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();
