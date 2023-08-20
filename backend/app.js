52ac9a8d8d7080a8cce0ae0fad64f2ce5e98f31a
tzarbasil <atreides03@mail.ru>
2023-08-20 12:15:33 +0200

    CORS fix

--- backend/app.js
+++ backend/app.js
@@ -60,14 +60,14 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
-  }
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();
