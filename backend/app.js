daf760fd5773571adceaac66ce12327e4a3bf48b
tzarbasil <atreides03@mail.ru>
2023-08-20 10:53:41 +0200

    CORS

--- backend/app.js
+++ backend/app.js
@@ -44,6 +44,26 @@ app.use((err, req, res, next) => {
 
 // app.use(helmet());
 
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'https://tzarbasil.nomoredomainsicu.ru',
+  'http://tzarbasil.nomoredomainsicu.ru',
+  'localhost:3000'
+];
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+      // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+      res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
